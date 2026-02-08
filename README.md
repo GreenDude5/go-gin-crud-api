17b93923506d9109ad5de398d3b81dc2a41471fe
GreenDude5 <stockipiotrek1@gmail.com>
2026-02-08 20:32:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Go Crud API
+
+# Technologies
+
+gin, gorm, postgres
+
+# How to run:
+
+```bash
+$ git clone https://github.com/GreenDude5/go-gin-crud-api
+$ go run main.go
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"go-gin-crud/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+}
